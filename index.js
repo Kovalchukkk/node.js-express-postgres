06d79168e3f0e522714fe2a91d0d3140dc7361ae
Mykola Kovalchuk <kolyankov@gmail.com>
2023-07-19 19:38:41 +0300

    Remove test GET handler and add userRouter into index.js file

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
+const userRouter = require("./routes/user-router");
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hey!");
-});
+app.use("/api", userRouter);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
