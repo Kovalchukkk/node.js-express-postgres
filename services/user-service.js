2b470c6afc08f8af973a420c7e9a6045b2b34e1c
Mykola Kovalchuk <kolyankov@gmail.com>
2023-07-19 19:02:27 +0300

    Add user-service.js file

---
+++ services/user-service.js
@@ -0,0 +1,30 @@
+const userRepository = require("../repositories/user-repository");
+
+class UserService {
+  async createUser(user) {
+    const createdUser = await userRepository.createUser(user);
+    return createdUser;
+  }
+
+  async getUsers() {
+    const users = await userRepository.getUsers();
+    return users;
+  }
+
+  async getOneUser(id) {
+    const user = await userRepository.getOneUser(id);
+    return user;
+  }
+
+  async updateUser(user) {
+    const updatedUser = await userRepository.updateUser(user);
+    return updatedUser;
+  }
+
+  async deleteUser(id) {
+    const deletedUser = await userRepository.deleteUser(id);
+    return deletedUser;
+  }
+}
+
+module.exports = new UserService();
