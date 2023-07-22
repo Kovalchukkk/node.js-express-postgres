0d413d73ede36cdca4cb68f19e03f8af0e1d4a71
Mykola Kovalchuk <kolyankov@gmail.com>
2023-07-22 15:58:06 +0300

    Add postRouter into index.js file

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
 const userRouter = require("./routes/user-router");
+const postRouter = require("./routes/post-router");
 
 const PORT = process.env.PORT || 8080;
 
@@ -9,5 +10,6 @@ const app = express();
 
 app.use(express.json());
 app.use("/api", userRouter);
+app.use("/api", postRouter);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
