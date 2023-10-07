1525f75e8cffbc1b8848fb1e22cb8cc8ad82887e
Mykola Kovalchuk <kolyankov@gmail.com>
2023-10-07 14:34:45 +0300

    Added table comment

--- database.sql
+++ database.sql
@@ -3,20 +3,26 @@ CREATE TABLE person(
     name VARCHAR(255) NOT NULL,
     surname VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE post(
     id SERIAL PRIMARY KEY NOT NULL,
     title VARCHAR(255) NOT NULL,
-    content VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
     person_id INTEGER NOT NULL,
-    FOREIGN KEY (person_id) REFERENCES person (id)
+    FOREIGN KEY (person_id) REFERENCES person (id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE role(
     id SERIAL PRIMARY KEY NOT NULL,
-    name VARCHAR(255) UNIQUE NOT NULL 
+    name VARCHAR(255) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
 );
 
 CREATE TABLE person_role(
@@ -24,5 +30,18 @@ CREATE TABLE person_role(
     person_id INTEGER NOT NULL,
     role_id INTEGER NOT NULL,
     FOREIGN KEY (person_id) REFERENCES person (id),
-    FOREIGN KEY (role_id) REFERENCES role (id)
+    FOREIGN KEY (role_id) REFERENCES role (id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE comment(
+    id SERIAL PRIMARY KEY NOT NULL,
+    content TEXT NOT NULL,
+    person_id INTEGER NOT NULL,
+    post_id INTEGER NOT NULL,
+    FOREIGN KEY (person_id) REFERENCES person (id),
+    FOREIGN KEY (post_id) REFERENCES post (id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
